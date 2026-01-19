ff6bff4e8f881725d609412ccb9c4bc31706dafe
Nandini Gupta <nandinigupta052005@gmail.com>
2026-01-20 02:03:02 +0530

    Add create-admin url

--- backend/ecommerce/urls.py
+++ backend/ecommerce/urls.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from store import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("store.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("create-admin/", views.create_admin, name="create_admin"),
+
 ]
 
 if settings.DEBUG:
